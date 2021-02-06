e7a7fa76d0081d061891423cf3537496ae7b9c28
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-06 14:20:20 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Article.destroy_all
+
+article1 = Article.create(title: "Today's news", description: "My first Blog")
+article2 = Article.create(title: "Top news", description: "My second Blog")
+article3 = Article.create(title: "ABC news", description: "My third Blog")
+article4 = Article.create(title: "Fox news", description: "My fourth Blog")
+article5 = Article.create(title: "ZME news", description: "My fifth Blog")
